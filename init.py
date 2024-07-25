8ae54c08a74b22514e23b32415e709187e477c77
iLoveJohnFish <1337minecrafter6969@gmail.com>
2024-07-25 03:22:17 -0400

    ITS SO BEAUTIFUL EVERYTHING IS SO BEAUTIFUL HOLY SHIT
    -build system finally works nicely across different windows systems
    
    -haven't tested for anything that isn't vs2022 and a 64-bit Windows machine lmao
    
    -switched everything to CMake, hopefully we can use CPack to create an installer for dll's that are needed to run the Peach Editor
    
    -revamped init.py to be more snazzy and catch errors, it also actually works nicely for building a CMake project!
    
    honestly it took like 15 hours to get this all working this nicely, but holy shit I am so happy that for someone to build Peach-E themselves, all they need to do is run:
    
    python init.py
    
    in their terminal and it just fucking works.
    
    it just works.

--- init.py
+++ init.py
@@ -1,19 +1,38 @@
 import subprocess
 
 def run_conan(build_type):
-    subprocess.run([
-        'conan', 'install', '.', '-s', 'build_type=Debug', '-s', 'compiler.cppstd=20', '--output-folder=build', "--build missing"
-    ])
+    try:
+        subprocess.run(
+            ['conan', 'install', '.', '-s', 'build_type=' + build_type, '-s', 'compiler.cppstd=20', '--output-folder=build', "--build=missing"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    except subprocess.CalledProcessError as e:
+        print("Failed to run Conan:")
+        print(e.stdout.decode())
+        print(e.stderr.decode())
 
 def run_cmake():
-    subprocess.run([
-        'cmake', '-S', '.', '-B', 'build'
-    ])
-    # subprocess.run([
-    #     'cmake', '--build', 'build'
-    # ])
+    try:
+        subprocess.run(
+            ['cmake', '-S', '.', '-B', 'build'],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        subprocess.run(
+            ['cmake', '--build', 'build'],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    except subprocess.CalledProcessError as e:
+        print("Failed to run CMake:")
+        print(e.stdout.decode())
+        print(e.stderr.decode())
 
 if __name__ == "__main__":
     run_conan("Debug")
-    #run_conan("Release")
+    #run_conan("Release")   >w>
     run_cmake()
