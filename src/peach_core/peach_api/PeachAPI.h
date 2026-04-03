bfdf49280bfd307bae44c2b72d09e0f229a6edd6
lessthen3 <1337minecrafter6969@gmail.com>
2026-04-02 20:02:13 -0400

    massive changes

--- src/peach_core/peach_api/PeachAPI.h
+++ src/peach_core/peach_api/PeachAPI.h
@@ -24,9 +24,9 @@
 #ifdef __cplusplus 
 
     //XXX: need to have C calling convention since this'll be called by external runtimes which probably just use a C calling convention esp C#
-    #if (defined(_WIN32) || defined(_WIN64)) && defined(PEACH_BUILD_DYNAMIC)
+    #if defined(PEACH_PLATFORM_WINDOWS) && defined(PEACH_BUILD_DYNAMIC)
             #define PEACH_API extern "C" __declspec(dllexport)
-    #elif (defined(_WIN32) || defined(_WIN64)) && !defined(PEACH_API_STATIC)
+    #elif defined(PEACH_PLATFORM_WINDOWS) && !defined(PEACH_API_STATIC)
             #define PEACH_API extern "C" __declspec(dllimport)
     #else
             #define PEACH_API extern "C"
