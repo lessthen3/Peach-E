7ecca43757620541e9a595f9ce9ee5391943f520
lessthen3 <1337minecrafter6969@gmail.com>
2026-04-05 16:57:17 -0400

    License updates UwU

--- src/peach_core/Scene-Items/Visual/SpotLight.h
+++ src/peach_core/Scene-Items/Visual/SpotLight.h
@@ -1,6 +1,6 @@
 ﻿/*******************************************************************
  *                        Peach-E v0.0.1
- *              Created by Ranyodh Mandur - 🍑 2024
+ *         Created by Ranyodh Singh Mandur - 🍑 2024-2026
  *
  *              Licensed under the MIT License (MIT).
  *         For more details, see the LICENSE file or visit:
