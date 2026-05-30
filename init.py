90d5c176997676fafee0b2dfb12381aa91b57f5e
lessthen3 <1337minecrafter6969@gmail.com>
2026-05-30 04:41:48 -0400

    added license to init.py cant believe i didnt do that before lmfao owo uwu uwu

--- init.py
+++ init.py
@@ -1,3 +1,13 @@
+##########################################################################
+#                        Peach-E v0.0.1
+#         Created by Ranyodh Singh Mandur - 🍑 2024-2026
+#
+#              Licensed under the MIT License (MIT).
+#         For more details, see the LICENSE file or visit:
+#               https://opensource.org/licenses/MIT
+#
+#           Peach-E is a free open source game engine
+##########################################################################
 import subprocess
 import os
 import argparse
